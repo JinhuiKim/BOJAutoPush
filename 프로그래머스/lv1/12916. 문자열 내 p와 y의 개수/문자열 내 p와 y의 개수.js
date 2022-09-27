75c19ff8af3b1f31a49906ac3c4f8140a765224f
journey <public.jinhui.kim@gmail.com>
2022-09-27 18:49:16 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -1,14 +1,3 @@
 function solution(s){
-  s = s.toLowerCase();
-
-  if (!s.includes("p") && !s.includes("y")) return true;
-
-  let pCount = 0,
-    yCount = 0;
-  for (const c of s) {
-    if (c !== "p" && c !== "y") continue;
-    c === "p" ? pCount++ : yCount++;
-  }
-
-  return pCount === yCount;
+    return s.match(/p/gi)?.length === s.match(/y/gi)?.length;
 }
