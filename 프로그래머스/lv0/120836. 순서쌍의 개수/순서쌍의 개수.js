e63e377bcd7f5be480798347a89b7b31a2402809
journey <public.jinhui.kim@gmail.com>
2023-01-24 05:00:52 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    if (n < 2) return 1;
+    
+    let answer = 0;
+    for (let i = 1; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) answer += 2; 
+    }
+    return Number.isInteger(Math.sqrt(n)) ? answer - 1 : answer;
+}
